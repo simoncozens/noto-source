dca60f71f689188a3c4a1d645041e32689663dc8
Simon Cozens <simon@simon-cozens.org>
2022-02-08 09:25:30 +0000

    Don't push yet; upstreamed some changes

--- scripts/notobuilder.py
+++ scripts/notobuilder.py
@@ -19,51 +19,30 @@ from gftools.builder.autohint import autohint
 
 
 class NotoBuilder(GFBuilder):
-    def __init__(self, sources):
-        family = self.get_family_name(sources[0])
-        self.config = {
-            "sources": sources,
-            "familyName": family,
-            "buildVariable": True,
-            "autohintTTF": False,  # We will, our own way
-            "buildWebfont": False,
-            "vfDir": "output/%s/unhinted/variable-ttf" % family,
-            "otDir": "output/%s/unhinted/otf" % family,
-            "ttDir": "output/%s/unhinted/ttf" % family,
-        }
-        self.outputs = set()
-        self.logger = logging.getLogger("GFBuilder")
-        self.fill_config_defaults()
+    def fill_config_defaults(self):
+        family = self.config["familyName"] = self.get_family_name()
+        if not "vfDir" in self.config:
+            self.config["vfDir"] = "output/%s/unhinted/variable-ttf" % family
+        if not "otDir" in self.config:
+            self.config["otDir"] = "output/%s/unhinted/otf" % family
+        if not "ttDir" in self.config:
+            self.config["ttDir"] "output/%s/unhinted/ttf" % family,
+        super().fill_config_defaults()
 
-    def get_family_name(self, source=None):
-        if not source:
-            source = self.config["sources"][0]
+    def get_family_name(self):
+        source = self.config["sources"][0]
         source, _ = os.path.splitext(os.path.basename(source))
         fname = re.sub(r"([a-z])([A-Z])", r"\1 \2", source)
         fname = re.sub("-?MM$", "", fname)
         return fname
 
     def post_process_ttf(self, filename):
-        super().post_process_ttf(filename)
-        self.outputs.add(filename)
+        self.logger.debug("Autohinting")
         hinted_dir = "output/%s/hinted/ttf" % self.config["familyName"]
         os.makedirs(hinted_dir, exist_ok=True)
         hinted = filename.replace("unhinted", "hinted")
-        try:
-            autohint(filename, hinted, add_script=True)
-            self.outputs.add(hinted)
-        except Exception as e:
-            self.logger.error("Couldn't autohint %s: %s" % (filename, e))
-
-    def post_process(self, filename):
-        super().post_process(filename)
-        self.outputs.add(filename)
-
-    def build_variable(self):
-        try:
-            super().build_variable()
-        except Exception as e:
-            self.logger.error("Couldn't build variable font: %s" % e)
+        autohint(filename, hinted, add_script=True)
+        self.post_process(filename)
 
 
 if __name__ == '__main__':
@@ -109,5 +88,5 @@ if __name__ == '__main__':
     builder.config["buildOTF"] = args.otf
     builder.build()
     print("Produced the following files:")
-    for o in builder.outputs:
+    for o in sorted(builder.outputs):
         print("* "+o)
